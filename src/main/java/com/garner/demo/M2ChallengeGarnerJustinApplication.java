f6d9dc58a6f9611d6018960db52726cd726e1ed4
Justin Garner <j.garner828@gmail.com>
2022-07-09 14:05:58 -0400

    created spring boot project files

---
+++ src/main/java/com/garner/demo/M2ChallengeGarnerJustinApplication.java
@@ -0,0 +1,13 @@
+package com.garner.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ChallengeGarnerJustinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ChallengeGarnerJustinApplication.class, args);
+	}
+
+}
